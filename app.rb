01dae45b3c72c786e0930c2fdcdedfa8f21f7f73
Carrotz45 <anisah.johansson@gmail.com>
2025-11-19 16:17:03 +0100

    började med sida, la upp databas och försöker lägga till add

--- app.rb
+++ app.rb
@@ -7,18 +7,38 @@ require 'sinatra/reloader'
 
 # Funktion för att prata med databasen
 # Exempel på användning: db.execute('SELECT * FROM fruits')
-def db
-  return @db if @db
+# def db
+#   return @db if @db
 
-  @db = SQLite3::Database.new(DB_PATH)
-  @db.results_as_hash = true
+#   @db = SQLite3::Database.new(DB_PATH)
+#   @db.results_as_hash = true
 
-  return @db
-end
+#   return @db
+# end
 
 # Routen /
-get '/' do
-    slim(:index)
+
+get('/') do
+
+  db = SQLite3::Database.new("db/todos.db")
+  db.results_as_hash = true
+  @todo_arr = db.execute("SELECT * FROM todos")
+  slim(:index)
+
 end
+
+post('/') do
+  query_name = params[:new_todo_name]
+  query_desc = params[:new_todo_desc]
+
+
+
+  db = SQLite3::Database.new("db/todos.db")
+
+  db.execute("INSERT INTO todos (name, description) VALUES(?,?)", [query_name, query_desc])
+  redirect('/')
+end
+
+
 
 
