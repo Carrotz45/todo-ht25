7a5bc9b2469381a107535330d28d66cf88d4b5d6
Carrotz45 <anisah.johansson@gmail.com>
2026-01-14 16:27:02 +0100

    la till register

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sqlite3'
 require 'slim'
 require 'sinatra/reloader'
+require 'BCrypt'
 
 
 
@@ -69,6 +70,50 @@ post('/:id/update') do
   db.execute("UPDATE todos SET name=?, description=? WHERE id=?",[name, description, id])
 
   redirect('/')
+end
+
+get('/register') do
+  slim(:register)
+end
+
+post('/register') do
+  db = SQLite3::Database.new('db/todos.db')
+  username = params["username"]
+  password = params["password"]
+  password_confirmation = params["confirm_password"]
+
+
+  result = db.execute("SELECT id FROM users WHERE username=?", username)
+
+  if result.empty?
+    if password == password_confirmation
+      password_digest = BCrypt::Password.create(password)
+      p password_digest
+      db.execute("INSERT INTO users(username, pwd_digest) VALUES (?,?)", [username, password_digest])
+      redirect('/')
+    else
+      #error not matching
+      p "not matcing"
+      redirect('/error')
+    end
+  else
+    #error results not filled out
+    p "user exists"
+    redirect('/error')
+  end
+
+end
+
+get ('/login') do
+
+  slim(:login)
+end
+
+post('/login') do
+  username = params["username"]
+  password = params["password"]
+
+  
 end
 
 
--- db/seeder.rb
+++ db/seeder.rb
@@ -16,6 +16,7 @@ end
 
 def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS todos')
+  db.execute('DROP TABLE IF EXISTS users')
 end
 
 def create_tables(db)
@@ -23,12 +24,15 @@ def create_tables(db)
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               name TEXT NOT NULL, 
               description TEXT)')
+  db.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, pwd_digest TEXT NOT NULL)')
 end
 
 def populate_tables(db)
   db.execute('INSERT INTO todos (name, description) VALUES ("Köp mjölk", "3 liter mellanmjölk, eko")')
   db.execute('INSERT INTO todos (name, description) VALUES ("Köp julgran", "En rödgran")')
   db.execute('INSERT INTO todos (name, description) VALUES ("Pynta gran", "Glöm inte lamporna i granen och tomten")')
+  db.execute('INSERT INTO users(username, pwd_digest) VALUES ("bob", "dijfsidjfi")')
+
 end
 
 seed!(db)
